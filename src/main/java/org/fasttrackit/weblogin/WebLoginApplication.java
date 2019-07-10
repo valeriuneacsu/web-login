88c43f320d20dbe3630f1b8c9ce47ba78981d280
valeriu.neacsu <valeriu.neacsu@yahoo.com>
2019-07-10 19:33:27 +0300

    Initial commit

---
+++ src/main/java/org/fasttrackit/weblogin/WebLoginApplication.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.weblogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebLoginApplication.class, args);
+	}
+
+}
